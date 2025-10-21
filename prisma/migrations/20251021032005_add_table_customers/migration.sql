2ed02247ce7a2bb49abbd18ed85e7d0db5165ba2
berdnaski <berdnaski1177@gmail.com>
2025-10-21 01:06:21 -0300

    feat: adding docker and prisma orm

---
+++ prisma/migrations/20251021032005_add_table_customers/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "phone" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_document_key" ON "Customer"("document");
