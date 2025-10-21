a4c9f31010975808523240c58ce9eb59c5450058
berdnaski <berdnaski1177@gmail.com>
2025-10-21 13:20:48 -0300

    feat: add scalar configure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +20,27 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+   const config = new DocumentBuilder()
+    .setTitle('Colmeia.io')
+    .setDescription('API de Pagamentos')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  app.use(
+    '/scalar',
+    apiReference({
+      content: document,
+      theme: 'nestjs',
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
